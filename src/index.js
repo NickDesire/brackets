fcdcdb8f1e79f205f2bec5969cae633ebc111d2e
Nikita <58188102+NickDesire@users.noreply.github.com>
2021-03-12 11:04:51 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-    if(str.length % 2 ==0){
-        return true;
+    if( str.length  % 2 == 0){
+        return true
     }
     else{
-        return false;
+        return false
     }
 }
